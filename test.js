232257d8d1cf9960e7ba732a631c265d45ddda08
O.Faruk <omerfarukcaki@gmail.com>
2019-11-20 12:31:23 +0300

    Update Readme

--- test.js
+++ test.js
@@ -7,6 +7,5 @@ console.log(toBinary.signed(-120));
 // 11111111111111111111111110001000
 // default length is 32 bits if not provided
 
-
-console.log(toBinary.unsigned(32,8));
+console.log(toBinary.unsigned(32, 8));
 // 00100000
